078f374e74e625aff6f417d6255b07c078e56d47
jlundy <jlundy@deeptrekker.com>
2023-03-12 12:02:11 -0400

    first pass at train location header

--- trainSimulator/trainlocation.h
+++ trainSimulator/trainlocation.h
@@ -1,11 +1,23 @@
 #ifndef TRAINLOCATION_H
 #define TRAINLOCATION_H
 
+#include "tracksegment.h"
+
+typedef enum {
+    SUCCESS,
+    HIT_TERMINAL,
+    DERAILED_AT_JUNCTION,
+    DERAILED_OFF_TRACK
+} train_motion_result;
 
 class TrainLocation
 {
 public:
-    TrainLocation();
+    TrainLocation(TrackSegment* track, float position);
+    train_motion_result increment(float distance);
+private:
+    TrackSegment* track;
+    float positionOnTrack; // measured from track 'rear-end'
 };
 
 #endif // TRAINLOCATION_H
