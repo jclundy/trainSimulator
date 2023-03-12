ca39be5d3b51e62e418f92bcdac0ca9f25e52bd2
jlundy <jlundy@deeptrekker.com>
2023-03-12 15:08:19 -0400

    adds virtual destructor for TrainControlModel

--- trainSimulator/traincontrolmodel.h
+++ trainSimulator/traincontrolmodel.h
@@ -5,6 +5,7 @@
 class TrainControlModel
 {
 public:
+    virtual ~TrainControlModel() {}
     virtual void computeNewStates(float &speed, float &acceleration, float speedSetpoint, float dt) = 0;
 };
 
