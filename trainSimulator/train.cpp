3bccc010a419d5eb7517f2d1c1d5c4c0842edf56
jlundy <jlundy@deeptrekker.com>
2023-03-12 11:53:22 -0400

    wip implementing train class

--- trainSimulator/train.cpp
+++ trainSimulator/train.cpp
@@ -1,6 +1,22 @@
 #include "train.h"
 
-Train::Train()
+Train::Train(float length)
 {
+    direction = 1;
+    // initialize front location
+    // initialize rear location
+    m_speed = 0;
+    m_acceleration = 0;
+    m_length = length;
+}
 
+void Train::place(TrackSegment *track, train_orientation orientation) {
+    QPointF midpoint = track->getCenter();
+
+    if(orientation == TRAIN_HEAD_TOWARDS_TRACK_FRONT) {
+        QPointF headPosition = midpoint + m_length/2;
+
+        // initialize front location to
+
+    }
 }
--- trainSimulator/train.h
+++ trainSimulator/train.h
@@ -5,14 +5,25 @@
 
 class TrainLocation;
 
+typedef enum {
+    TRACK_FORWARD,
+    TRACK_REVERSE
+} train_direction;
+
+typedef enum {
+    TRAIN_HEAD_TOWARDS_TRACK_FRONT,
+    TRAIN_HEAD_TOWARDS_TRACK_REAR
+} train_orientation;
+
 class Train
 {
 public:
-    Train();
-    void place(TrackSegment* track, bool forward);
+    Train(float length = 5);
+    void place(TrackSegment* track, train_orientation orientation);
     void drive();
     void setDirection();
     void stop();
+    void setSpeed();
 
 private:
     int direction; // -1 reverse, 0 neutral, 1 forward
