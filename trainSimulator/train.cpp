d2bfc5e85e237933e878fd54760990bb3668e9e6
jlundy <jlundy@deeptrekker.com>
2023-03-12 12:56:59 -0400

    implements train's place function

--- trainSimulator/train.cpp
+++ trainSimulator/train.cpp
@@ -11,12 +11,19 @@ Train::Train(float length)
 }
 
 void Train::place(TrackSegment *track, train_orientation orientation) {
-    QPointF midpoint = track->getCenter();
+
+    float midpoint = track->getLength()/2;
+    float headPosition = 0;
+    float rearPosition = 0;
 
     if(orientation == TRAIN_HEAD_TOWARDS_TRACK_FRONT) {
-//        QPointF headPosition = midpoint + m_length/2;
-
-        // initialize front location to
-
+        headPosition = midpoint + m_length/2;
+        rearPosition = midpoint - m_length/2;
+    } else {
+        headPosition = midpoint - m_length/2;
+        rearPosition = midpoint + m_length/2;
     }
+
+    frontLocation.resetPosition(track, headPosition);
+    rearLocation.resetPosition(track, rearPosition);
 }
