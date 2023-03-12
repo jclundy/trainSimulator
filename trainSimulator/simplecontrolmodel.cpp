ce8b4af5b54019defc3a4080a3b32396c846a3f7
jlundy <jlundy@deeptrekker.com>
2023-03-12 14:40:48 -0400

    first pass at simplecontrolmodel

--- trainSimulator/simplecontrolmodel.cpp
+++ trainSimulator/simplecontrolmodel.cpp
@@ -1,6 +1,60 @@
 #include "simplecontrolmodel.h"
+#include <math.h>
 
-SimpleControlModel::SimpleControlModel()
+SimpleControlModel::SimpleControlModel(float maxSpeed, float maxAcceleration, float maxBraking, float errorThreshold)
 {
-
+    m_maxSpeed = maxSpeed;
+    m_maxAcceleration = maxAcceleration;
+    m_maxBraking = maxBraking;
+    m_errorThreshold = errorThreshold;
 }
+
+void SimpleControlModel::computeNewStates(float &speed, float &acceleration, float speedSetpoint, float dt) {
+    // if speed is greater than max speed, slow it down
+    if(speedSetpoint > m_maxSpeed) {
+        speedSetpoint = m_maxSpeed;
+    }
+
+    float setpointError = (speedSetpoint - speed);
+
+    // if we are at our setpoint speed, stop accelerating
+    if(fabs(setpointError) < m_errorThreshold) {
+        acceleration = 0;
+        speed = speedSetpoint;
+    }
+    // situation where engine is driving
+    else {
+        acceleration = m_maxAcceleration * copysign(1.0, setpointError);
+    }
+    // update speed
+    speed = speed + dt*acceleration;
+}
+
+void SimpleControlModel::computeNewStatesBraking(float &speed, float &acceleration, float speedSetpoint, float dt) {
+    // if speed is greater than max speed, slow it down
+    if(speedSetpoint > m_maxSpeed) {
+        speedSetpoint = m_maxSpeed;
+    }
+
+    float setpointError = (speedSetpoint - speed);
+
+    // if we are at our setpoint speed, stop accelerating
+    if(fabs(setpointError) < m_errorThreshold) {
+        acceleration = 0;
+        speed = speedSetpoint;
+    }
+    // situation where we are braking - speed and setpoint are same sign
+    else if((speedSetpoint < speed) && (speedSetpoint > 0 && speed > 0)) {
+        acceleration = -m_maxBraking;
+    } else if ((speedSetpoint > speed) && (speedSetpoint < 0 && speed < 0)) {
+        acceleration = m_maxBraking;
+    }
+    // situation where engine is driving
+    else {
+        acceleration = m_maxAcceleration * copysign(1.0, setpointError);
+    }
+    // update speed
+    speed = speed + dt*acceleration;
+}
+
+
--- trainSimulator/simplecontrolmodel.h
+++ trainSimulator/simplecontrolmodel.h
@@ -1,11 +1,19 @@
 #ifndef SIMPLECONTROLMODEL_H
 #define SIMPLECONTROLMODEL_H
 
+#include "traincontrolmodel.h"
 
-class SimpleControlModel
+class SimpleControlModel : public TrainControlModel
 {
 public:
-    SimpleControlModel();
+    SimpleControlModel(float maxSpeed = 4, float maxAcceleration = 1, float maxBraking = 2, float errorThreshold = 10e-3);
+    void computeNewStates(float &speed, float &acceleration, float speedSetpoint, float dt) override;
+    void computeNewStatesBraking(float &speed, float &acceleration, float speedSetpoint, float dt);
+private:
+    float m_maxSpeed; // m/s
+    float m_maxAcceleration; // m/s^2
+    float m_maxBraking; // m/s^2
+    float m_errorThreshold;
 };
 
 #endif // SIMPLECONTROLMODEL_H
