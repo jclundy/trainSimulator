a583d2ad5fd1d580d0d5af880f014b2bad6ea278
jlundy <jlundy@deeptrekker.com>
2023-03-11 22:51:41 -0500

    first pass at train class header

--- trainSimulator/train.h
+++ trainSimulator/train.h
@@ -1,11 +1,27 @@
 #ifndef TRAIN_H
 #define TRAIN_H
 
+#include "tracksegment.h"
+
+class TrainLocation;
 
 class Train
 {
 public:
     Train();
+    void place(TrackSegment* track, bool forward);
+    void drive();
+    void setDirection();
+    void stop();
+
+private:
+    int direction; // -1 reverse, 0 neutral, 1 forward
+    float m_speed;
+    float m_acceleration;
+    float m_length;
+
+    TrainLocation frontLocation;
+    TrainLocation rearLocation;
 };
 
 #endif // TRAIN_H
