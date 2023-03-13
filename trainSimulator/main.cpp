3f24375a8bb5c6c089d6ec1d5a1e6f9ba44a35c1
jlundy <jlundy@deeptrekker.com>
2023-03-12 21:26:18 -0400

    runs test in main

--- trainSimulator/main.cpp
+++ trainSimulator/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include "tests/traintest.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    TrainTest mainTest;
+    mainTest.testSetup();
 
     return a.exec();
 }
