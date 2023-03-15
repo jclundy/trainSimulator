e3974f692c485cd709d3f65eea3e8993fbda0018
jlundy <jlundy@deeptrekker.com>
2023-03-15 08:57:51 -0400

    first pass at implementing track geometry header

--- trainSimulator/track_components/trackgeometry.h
+++ trainSimulator/track_components/trackgeometry.h
@@ -1,11 +1,30 @@
 #ifndef TRACKGEOMETRY_H
 #define TRACKGEOMETRY_H
 
+#include <QPointF>
+#include <QLineF>
 
 class TrackGeometry
 {
 public:
-    TrackGeometry();
+    TrackGeometry(float length, const QPointF &position);
+
+    //getters
+    QPointF getFrontEndPosition();
+    QPointF getRearEndPosition();
+    QPointF getCenter();
+
+    // modifiers
+    void setCenter(const QPointF &newCenter);
+    void translate(const QPointF &offset);
+    void setRotationAboutCenter(float degrees);
+    void setRotationAboutFront(float degrees);
+    void setRotationAboutRear(float degrees);
+    void setForwardPosition(QPointF position);
+    void setRearPosition(QPointF position);
+private:
+    // public members
+    QLineF m_lineSegment;
 };
 
 #endif // TRACKGEOMETRY_H
