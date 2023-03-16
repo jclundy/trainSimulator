84f47b440753e767ee1bcc8560cecf1e5f0b0ed3
jlundy <jlundy@deeptrekker.com>
2023-03-15 22:42:06 -0400

    first pass at signal header

--- trainSimulator/track_system/signal.h
+++ trainSimulator/track_system/signal.h
@@ -1,11 +1,25 @@
 #ifndef SIGNAL_H
 #define SIGNAL_H
 
+#include <track_components/itracksegment.h>
 
 class Signal
 {
 public:
     Signal();
+    bool isRed();
+    bool isGreen();
+    void setRed();
+    void setGreen();
+    void setState(bool state);
+    ITrackSegment* getTrackSegment();
+
+    void placeOnTrackSegmentFront(ITrackSegment*);
+    void placeOnTrackSegmentRear(ITrackSegment*);
+
+private:
+    bool m_state; // true - green; red - false
+    ITrackSegment* m_trackSegment;
 };
 
 #endif // SIGNAL_H
