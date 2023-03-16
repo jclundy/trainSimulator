eab2a521fcac963cba00e2fdeb44127dbdb20e39
jlundy <jlundy@deeptrekker.com>
2023-03-15 22:47:40 -0400

    implements getters and setters for signal

--- trainSimulator/track_system/signal.cpp
+++ trainSimulator/track_system/signal.cpp
@@ -2,5 +2,35 @@
 
 Signal::Signal()
 {
+    m_state = true;
+    m_trackSegment = NULL;
+    m_position = SIGNAL_UNPLACED;
+}
 
+bool Signal::isRed() {
+    return m_state == false;
+}
+
+bool Signal::isGreen() {
+    return m_state;
+}
+
+void Signal::setRed() {
+    setState(false);
+}
+
+void Signal::setGreen() {
+    setState(true);
+}
+
+void Signal::setState(bool state) {
+    m_state = state;
+}
+
+ITrackSegment* Signal::getTrackSegment() {
+    return m_trackSegment;
+}
+
+signal_position Signal::getPosition() {
+    return m_position;
 }
--- trainSimulator/track_system/signal.h
+++ trainSimulator/track_system/signal.h
@@ -3,6 +3,12 @@
 
 #include <track_components/itracksegment.h>
 
+typedef enum {
+    SIGNAL_UNPLACED,
+    SIGNAL_TRACK_FRONT,
+    SIGNAL_TRACK_REAR
+} signal_position;
+
 class Signal
 {
 public:
@@ -13,12 +19,14 @@ public:
     void setGreen();
     void setState(bool state);
     ITrackSegment* getTrackSegment();
+    signal_position getPosition();
 
     void placeOnTrackSegmentFront(ITrackSegment*);
     void placeOnTrackSegmentRear(ITrackSegment*);
 
 private:
     bool m_state; // true - green; red - false
+    signal_position m_position;
     ITrackSegment* m_trackSegment;
 };
 
