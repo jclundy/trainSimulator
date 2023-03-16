734531198a7fbcbaea300fae5fb8c2503ea0097f
jlundy <jlundy@deeptrekker.com>
2023-03-16 11:03:34 -0400

    runs track system test in main.cpp

--- trainSimulator/main.cpp
+++ trainSimulator/main.cpp
@@ -2,6 +2,10 @@
 #include <QApplication>
 
 #include "tests/traintest.h"
+#include "tests/tracksystemtest.h"
+
+#define RUN_BASIC_TEST 0
+#define RUN_SYSTEM_TEST 1
 
 int main(int argc, char *argv[])
 {
@@ -9,8 +13,15 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
+#if RUN_BASIC_TEST
     TrainTest mainTest;
     mainTest.runTests();
+#endif
+
+#if RUN_SYSTEM_TEST
+    TrackSystemTest test;
+    test.testSetup();
+#endif
 
     return a.exec();
 }
