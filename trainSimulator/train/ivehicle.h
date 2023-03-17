fdcbb8ab46a5e4e47464313212f71a809b526d49
jlundy <jlundy@deeptrekker.com>
2023-03-17 16:11:37 -0400

    defines IVehicle interface

--- trainSimulator/train/ivehicle.h
+++ trainSimulator/train/ivehicle.h
@@ -1,4 +1,11 @@
 #ifndef IVEHICLE_H
 #define IVEHICLE_H
 
+class IVehicle {
+    ~IVehicle() {}
+    virtual unsigned int getId() = 0;
+    virtual int getPriority() = 0;
+    virtual float getSpeed() = 0;
+};
+
 #endif // IVEHICLE_H
