0e72ee2a35b8db14b58e0b1916af2254ad284199
jlundy <jlundy@deeptrekker.com>
2023-03-16 23:28:21 -0400

    first pass at implementing track sensor

--- trainSimulator/track_components/tracksensor.cpp
+++ trainSimulator/track_components/tracksensor.cpp
@@ -1,6 +1,66 @@
 #include "tracksensor.h"
 
-TrackSensor::TrackSensor()
-{
+#include <math.h>
 
+TrackSensor::TrackSensor(float positionOnTrack, float timeout)
+{
+    m_positionOnTrack = positionOnTrack;
+    m_timeoutSetting = timeout; //seconds
+    reset();
+}
+
+bool TrackSensor::isTrainPresent() {
+    return m_trainPresent;
+}
+
+int TrackSensor::getTrainId() {
+    return m_trainId;
+}
+
+int TrackSensor::getTrainPriority() {
+    return m_trainPriority;
+}
+
+float TrackSensor::getTrainSpeed() {
+    return m_trainSpeed;
+}
+
+float TrackSensor::getTrainPositionOnTrack() {
+    return m_trainPositionOnTrack;
+}
+
+void TrackSensor::reset() {
+    m_trainId = -1;
+    m_trainPresent = false;
+    m_trainPriority = 0;
+    m_trainPositionOnTrack = 0;
+    m_countDown = 0;
+}
+
+void TrackSensor::trigger(Train* train, TrainLocation * trackLocation) {
+    // reset countdown
+    m_countDown = m_timeoutSetting;
+    m_trainPresent = true;
+
+    if(isTrainPresent() == false ) {
+        // if no train previously present, just trigger the sensor
+        updateTrainInfo(train, trackLocation);
+    } else if (m_trainId == (int)train->getId()) {
+        // if it is the same train as before keep triggering the sensor
+        updateTrainInfo(train, trackLocation);
+    } else {
+        float currentDistanceToSensor = m_trainPositionOnTrack - m_positionOnTrack;
+        float otherTrainsDistanceToSensor = trackLocation->getPositionOnTrack() - m_positionOnTrack;
+
+        if(fabs(currentDistanceToSensor) > fabs(otherTrainsDistanceToSensor)) {
+            updateTrainInfo(train, trackLocation);
+        }
+    }
+}
+
+void TrackSensor::updateTrainInfo(Train* train, TrainLocation * trackLocation) {
+    m_trainId = train->getId();
+    m_trainPriority = train->getPriority();
+    m_trainSpeed = train->getSpeed();
+    m_trainPositionOnTrack = trackLocation->getPositionOnTrack();
 }
--- trainSimulator/track_components/tracksensor.h
+++ trainSimulator/track_components/tracksensor.h
@@ -1,11 +1,39 @@
 #ifndef TRACKSENSOR_H
 #define TRACKSENSOR_H
 
+#include "train/train.h"
 
 class TrackSensor
 {
 public:
-    TrackSensor();
+    TrackSensor(float positionOnTrack, float timeout = 5);
+
+    // getters
+    bool isTrainPresent();
+    int getTrainId();
+    int getTrainPriority();
+    float getTrainSpeed();
+    float getTrainPositionOnTrack();
+
+    // setters
+    void reset();
+    void trigger(Train* train, TrainLocation * trackLocation);
+    void countDown(float dt);
+
+private:
+
+    void updateTrainInfo(Train* train, TrainLocation * trackLocation);
+
+    int m_positionOnTrack;
+    bool m_trainPresent;
+    int m_trainId;
+    int m_trainPriority;
+
+    float m_trainSpeed;
+    float m_trainPositionOnTrack;
+    float m_timeoutSetting;
+    float m_countDown;
+
 };
 
 #endif // TRACKSENSOR_H
