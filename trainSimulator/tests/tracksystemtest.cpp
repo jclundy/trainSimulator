8e41d5c5dd268812e2d246ea6049b83faa4b7aa1
jlundy <jlundy@deeptrekker.com>
2023-03-16 21:55:23 -0400

    removes call to manually drive signals in track system test

--- trainSimulator/tests/tracksystemtest.cpp
+++ trainSimulator/tests/tracksystemtest.cpp
@@ -158,7 +158,6 @@ void TrackSystemTest::testSignals() {
     JunctionTrack* track1 = m_trackSystem->getJunctionById(1);
     track1->selectRearBranchById(0);
     track1->selectForwardBranchById(2);
-    m_trackSystem->driveSignals();
 
     printSignalInfo(sig0);
     printSignalInfo(sig2);
@@ -169,7 +168,6 @@ void TrackSystemTest::testSignals() {
     qDebug() << "Selecting rear branch track4 and forward branch track 5";
     track1->selectRearBranchById(4);
     track1->selectForwardBranchById(5);
-    m_trackSystem->driveSignals();
 
     printSignalInfo(sig0);
     printSignalInfo(sig2);
