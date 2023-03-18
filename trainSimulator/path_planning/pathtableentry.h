e7ece18a8e70cbdecb793f5d7efa00d74858dcd0
jlundy <jlundy@deeptrekker.com>
2023-03-18 17:46:29 -0400

    first pass at pathtableentry header

--- trainSimulator/path_planning/pathtableentry.h
+++ trainSimulator/path_planning/pathtableentry.h
@@ -5,7 +5,27 @@
 class PathTableEntry
 {
 public:
-    PathTableEntry();
+    PathTableEntry(unsigned int id, float trackLength);
+    void update(unsigned int neighbourId, float neighbourDistance);
+
+    bool getVisited();
+    bool setVisited();
+
+    unsigned int getTrackId();
+    float getTrackLength();
+    float getDistanceToTarget();
+    float getNextId();
+    int getDirectionToNext();
+
+private:
+    unsigned int m_trackId;
+    float m_trackLength;
+    float m_distanceToTarget;
+
+    unsigned int m_nextId;
+    int m_directionToNext; //-1=reverse, 1=forward, 0=stop
+
+    bool m_visited;
 };
 
 #endif // PATHTABLEENTRY_H
