2768b748cb3f674954f666ccf38e6539332589dc
jlundy <jlundy@deeptrekker.com>
2023-03-18 10:00:56 -0400

    wip implementing collision checker

--- trainSimulator/track_system/collisionchecker.cpp
+++ trainSimulator/track_system/collisionchecker.cpp
@@ -1,6 +1,71 @@
 #include "collisionchecker.h"
 
-CollisionChecker::CollisionChecker()
-{
+bool CollisionChecker::collisionWillOccur(ITrackSegment* rearTrack, ITrackSegment* frontTrack) {
 
+    train_speed_data rearTrain = getRearTrackTrainSpeed(rearTrack);
+    train_speed_data frontTrain = getFrontTrackTrainSpeed(frontTrack);
+
+    if(rearTrain.trainPresent && frontTrain.trainPresent && rearTrain.trainId != frontTrain.trainId) {
+        return rearTrain.trainSpeed> frontTrain.trainSpeed;
+    }
+    return false;
+}
+
+bool CollisionChecker::collisionWillOccurInForwardDirection(ITrackSegment* rearTrack, ITrackSegment* frontTrack) {
+    return false;
+}
+
+bool CollisionChecker::collisionWillOccurInReverseDirection(ITrackSegment* rearTrack, ITrackSegment* frontTrack) {
+    return false;
+}
+
+bool CollisionChecker::trainBlockingForward(ITrackSegment* currentTrack) {
+    ITrackSegment* frontTrack = currentTrack->getSelectedForwardEnd();
+    train_speed_data frontTrain = getFrontTrackTrainSpeed(frontTrack);
+
+    if(frontTrain.trainPresent && frontTrain.trainSpeed <= 0) {
+        return true;
+    }
+    return false;
+}
+
+bool CollisionChecker::trainBlockingRear(ITrackSegment* currentTrack) {
+    ITrackSegment* rearTrack = currentTrack->getSelectedRearEnd();
+    train_speed_data rearTrain = getRearTrackTrainSpeed(rearTrack);
+
+    if(rearTrain.trainPresent && rearTrain.trainSpeed >= 0) {
+        return true;
+    }
+    return false;
+}
+
+
+// should be named get train speed of train closest to rear
+train_speed_data CollisionChecker::getRearTrackTrainSpeed(ITrackSegment* rearTrack) {
+    train_speed_data trainSpeed = default_train_speed_data;
+
+    if(rearTrack != NULL) {
+        TrackSensor* closestRearSensor = rearTrack->getFrontSensor();
+        if(closestRearSensor != NULL) {
+            trainSpeed.trainPresent = closestRearSensor->isTrainPresent();
+            trainSpeed.trainId = closestRearSensor->getTrainId();
+            trainSpeed.trainSpeed = closestRearSensor->getTrainSpeed();
+        }
+    }
+
+    return trainSpeed;
+}
+
+// should be named get train speed of train closest to front
+train_speed_data CollisionChecker::getFrontTrackTrainSpeed(ITrackSegment* frontTrack) {
+    train_speed_data trainSpeed = default_train_speed_data;
+    if(frontTrack != NULL) {
+        TrackSensor* closestForwardSensor = frontTrack->getRearSensor();
+        if(closestForwardSensor != NULL) {
+            trainSpeed.trainPresent = closestForwardSensor->isTrainPresent();
+            trainSpeed.trainId = closestForwardSensor->getTrainId();
+            trainSpeed.trainSpeed = closestForwardSensor->getTrainSpeed();
+        }
+    }
+    return trainSpeed;
 }
--- trainSimulator/track_system/collisionchecker.h
+++ trainSimulator/track_system/collisionchecker.h
@@ -1,11 +1,34 @@
 #ifndef COLLISIONCHECKER_H
 #define COLLISIONCHECKER_H
 
+#include "track_components/itracksegment.h"
+#include "train/ivehicle.h"
+
+typedef struct {
+    bool trainPresent;
+    float trainSpeed;
+    int trainId;
+} train_speed_data;
+
+static const train_speed_data default_train_speed_data = {
+    .trainPresent = false,
+    .trainSpeed = 0,
+    .trainId = -1
+};
 
 class CollisionChecker
 {
 public:
-    CollisionChecker();
+    static bool collisionWillOccur(ITrackSegment* rearTrack, ITrackSegment* frontTrack);
+    static bool collisionWillOccurInForwardDirection(ITrackSegment* rearTrack, ITrackSegment* frontTrack);
+    static bool collisionWillOccurInReverseDirection(ITrackSegment* rearTrack, ITrackSegment* frontTrack);
+
+    static bool trainBlockingForward(ITrackSegment* currentTrack);
+    static bool trainBlockingRear(ITrackSegment* currentTrack);
+    static bool collisionOccured(IVehicle* train1, IVehicle* train2);
+
+    static train_speed_data getRearTrackTrainSpeed(ITrackSegment* rearTrack);
+    static train_speed_data getFrontTrackTrainSpeed(ITrackSegment* frontTrack);
 };
 
 #endif // COLLISIONCHECKER_H
