4fc431274182d4b592abb391504d1efc79ce6260
jlundy <jlundy@deeptrekker.com>
2023-03-18 16:01:01 -0400

    first pass at trackpathtable header

--- trainSimulator/path_planning/trackpathtable.h
+++ trainSimulator/path_planning/trackpathtable.h
@@ -1,11 +1,33 @@
 #ifndef TRACKPATHTABLE_H
 #define TRACKPATHTABLE_H
 
+#include "track_components/itracksegment.h"
+#include <QList>
+#include <QMap>
+
+typedef struct {
+    unsigned int trackId;
+    int trackLength;
+    unsigned int nextId;
+    int directionToNext; //-1=reverse, 1=forward, 0=stop
+} path_table_entry;
+
+typedef struct {
+    unsigned int trackId;
+    int directionToNext; //-1=reverse, 1=forward, 0=stop
+} path_step;
 
 class TrackPathTable
 {
 public:
     TrackPathTable();
+    void initialize(QList<ITrackSegment*> trackList, unsigned int targetId);
+    void computeTable();
+    int getDirectionToNext(unsigned int trackId);
+    QList<path_step> getPathListFrom(unsigned int trackId);
+
+private:
+    QMap<unsigned int, path_table_entry> m_table;
 };
 
 #endif // TRACKPATHTABLE_H
