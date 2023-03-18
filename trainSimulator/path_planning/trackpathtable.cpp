32cb9ad44666ceeff606ca730a46a4c1a1a574fd
jlundy <jlundy@deeptrekker.com>
2023-03-18 16:08:33 -0400

    adds empty implementations to trackpathtable.cpp

--- trainSimulator/path_planning/trackpathtable.cpp
+++ trainSimulator/path_planning/trackpathtable.cpp
@@ -4,3 +4,20 @@ TrackPathTable::TrackPathTable()
 {
 
 }
+
+void TrackPathTable::initialize(QList<ITrackSegment*> trackList, unsigned int targetId) {
+
+}
+
+void TrackPathTable::computeTable() {
+
+}
+
+int TrackPathTable::getDirectionToNext(unsigned int trackId) {
+    return 0;
+}
+
+QList<path_step> TrackPathTable::getPathListFrom(unsigned int trackId) {
+    QList<path_step> pathList;
+    return pathList;
+}
