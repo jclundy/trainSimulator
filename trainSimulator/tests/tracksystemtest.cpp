9182cc31f2e48e5fc602cd05d01f630d30ef067c
jlundy <jlundy@deeptrekker.com>
2023-03-18 20:26:24 -0400

    adds test for path planning

--- trainSimulator/tests/tracksystemtest.cpp
+++ trainSimulator/tests/tracksystemtest.cpp
@@ -1,5 +1,8 @@
 #include "tracksystemtest.h"
 #include "testutils.h"
+
+#include "path_planning/trackpathtable.h"
+
 #include <QDebug>
 
 TrackSystemTest::TrackSystemTest()
@@ -9,8 +12,9 @@ TrackSystemTest::TrackSystemTest()
 
 void TrackSystemTest::runTests() {
     testSetup();
-    testSignals();
-    testDrive1();
+//    testSignals();
+//    testDrive1();
+    testPathPlanning();
 
 }
 
@@ -250,6 +254,31 @@ void TrackSystemTest::testSignals() {
 
 }
 
+
+void TrackSystemTest::testPathPlanning() {
+    qDebug() << "========================================";
+    qDebug() << "Path Planning Test";
+
+    qDebug() << "Creating new path table";
+    TrackPathTable *table = new TrackPathTable();
+
+    qDebug() << "Initializing table";
+    table->initialize(m_trackSystem, 5);
+
+    qDebug() << "Computing table";
+    table->computeTable();
+
+    qDebug() << "---------------------------------------";
+    qDebug() << "Path from 0";
+    QList<path_step> pathFromTrack0 = table->getPathListFrom(0);
+    printTrackPath(pathFromTrack0);
+
+    qDebug() << "---------------------------------------";
+    qDebug() << "Path from 3";
+    QList<path_step> pathFromTrack3 = table->getPathListFrom(3);
+    printTrackPath(pathFromTrack3);
+}
+
 void TrackSystemTest::printAllSignalInfo() {
     for(int i = 0; i < m_trackSystem->getSignals().size(); i++) {
         printSignalInfo(m_trackSystem->getSignals().at(i));
@@ -260,5 +289,12 @@ void TrackSystemTest::printTrackSystemInfo() {
     QList<ITrackSegment *> trackList = m_trackSystem->getTrackSegments();
     for (int i = 0; i < trackList.size(); i++) {
         printTrackInfo(trackList.at(i));
+    }
+}
+
+void TrackSystemTest::printTrackPath(QList<path_step> pathList) {
+    for (int i = 0; i < pathList.size(); i++) {
+        path_step step = pathList.at(i);
+        qDebug() << "Track: " << step.trackId << "; Next " << step.nextTrackId << " direction " << step.directionToNext;
     }
 }
