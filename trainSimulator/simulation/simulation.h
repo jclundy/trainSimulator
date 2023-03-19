3f11b26aa5a5328ede69988e7f7a4aa81dc784a5
jlundy <jlundy@deeptrekker.com>
2023-03-19 09:27:56 -0400

    first pass at simulation header

--- trainSimulator/simulation/simulation.h
+++ trainSimulator/simulation/simulation.h
@@ -2,6 +2,10 @@
 #define SIMULATION_H
 
 #include <QObject>
+#include <QTimer>
+
+#include "track_system/tracksystem.h"
+#include "track_system/systemcontroller.h"
 
 class Simulation : public QObject
 {
@@ -9,9 +13,26 @@ class Simulation : public QObject
 public:
     explicit Simulation(QObject *parent = nullptr);
 
+    // getters
+    TrackSystem* getTrackSystem();
+    // initialization
+    void loadTrackSystem();
+    void loadTrainDestinations();
+
+    void setTimeout(float timeout); // in seconds
+
+    // running simulation
+    void startSimulation();
+    void stopSimulation();
+
 signals:
 
 public slots:
+
+private:
+    TrackSystem* m_trackSystem;
+    SystemController* m_controller;
+    QTimer m_timer;
 };
 
 #endif // SIMULATION_H
