cde561c3cf5c5ba20e2e32da726694ffe8311984
jlundy <jlundy@deeptrekker.com>
2023-03-19 11:28:33 -0400

    first pass at logger header

--- trainSimulator/simulation/simulationlogger.h
+++ trainSimulator/simulation/simulationlogger.h
@@ -1,11 +1,26 @@
 #ifndef SIMULATIONLOGGER_H
 #define SIMULATIONLOGGER_H
 
+#include <QTextStream>
+#include "track_system/tracksystem.h"
+#include "track_system/systemcontroller.h"
 
 class SimulationLogger
 {
+    SimulationLogger(QTextStream out);
 public:
-    SimulationLogger();
+    // high-level logging
+    void logTrackSystemInfo(TrackSystem* system);
+    void logTrainPaths(SystemController* system);
+
+    // logging for individual components
+    void logTrackInfo(ITrackSegment *track);
+    void logSignalInfo(Signal* signal);
+    void logJunctionInfo(Junction* junction);
+    void logTrainInfo(Train* train);
+
+private:
+    QTextStream m_out;
 };
 
 #endif // SIMULATIONLOGGER_H
