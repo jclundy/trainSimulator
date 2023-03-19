b6be3134f87fb608e083adedaf438ef6f82b0e47
jlundy <jlundy@deeptrekker.com>
2023-03-18 22:47:58 -0400

    first pass at system controller header

--- trainSimulator/track_system/systemcontroller.h
+++ trainSimulator/track_system/systemcontroller.h
@@ -1,11 +1,26 @@
 #ifndef SYSTEMCONTROLLER_H
 #define SYSTEMCONTROLLER_H
 
+#include "train/train.h"
+#include "path_planning/trackpathtable.h"
+#include "track_components/junction.h"
+#include "track_system/tracksystem.h"
+
+#include <QMap>
 
 class SystemController
 {
 public:
-    SystemController();
+    SystemController(TrackSystem* system);
+
+    void setAllTrainDestinations(QMap<unsigned int, unsigned int>); // key - trainId, value - trackId
+    void setTrainDestination(int trainId, int destinationId);
+    void controlTrains();
+    void controlJunctions();
+
+private:
+    TrackSystem* m_trackSystem;
+    QMap<unsigned int, TrackPathTable*> m_trainPaths;
 };
 
 #endif // SYSTEMCONTROLLER_H
